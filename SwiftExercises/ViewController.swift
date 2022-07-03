df1641073d869a2c64e72a46826ccfaa34e65313
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-07-03 16:37:21 +0300

    Working with UserDefaults

--- SwiftExercises/ViewController.swift
+++ SwiftExercises/ViewController.swift
@@ -12,16 +12,46 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var txtUserName: UITextField!
     @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var btnRemove: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lblResult.text?.removeAll()
-
+        btnRemove.isHidden=true
     }
 
    
+    @IBAction func btnGetData(_ sender: Any) {
+        btnRemove.isHidden=false
+        /*
+         ------USING CAST-----
+         
+        let lastSavedText=UserDefaults.standard.object(forKey: "userName")
+        
+        if let lastSavedText = lastUserName as? String{
+            lblResult.text="Last saved text: \(lastSavedText)"
+        }
+        */
+        
+        if let lastSavedText = UserDefaults.standard.string(forKey: "userName"){
+            lblResult.text="Last saved text: \(lastSavedText)"
+        }else{
+            lblResult.text="There is no any saved text yet!"
+        }
+    }
+    
+    @IBAction func btnRemoveClicked(_ sender: Any) {
+        if (UserDefaults.standard.string(forKey: "userName") != nil){
+            UserDefaults.standard.removeObject(forKey:"userName")
+            lblResult.text="UserName removed"
+        }
+    }
     @IBAction func btnSaveClicked(_ sender: Any) {
-        lblResult.text=txtUserName.text
+        
+        UserDefaults.standard.set(txtUserName.text, forKey: "userName")
+        txtUserName.text?.removeAll()
+        btnRemove.isHidden=true
+        lblResult.text="Result: Saved"
     }
     
     
