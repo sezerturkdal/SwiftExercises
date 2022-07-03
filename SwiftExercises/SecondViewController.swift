bfdfd2131ddcc2b1cf9dc66c05d872be6245a77b
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-07-03 17:28:56 +0300

    SecondViewController added

---
+++ SwiftExercises/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  SwiftExercises
+//
+//  Created by Sezer on 3.07.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var lblScreenName: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
