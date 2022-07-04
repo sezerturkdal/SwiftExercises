57ce6de5aeec0e93453d336118c006ab3f3a41eb
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-07-04 21:40:23 +0300

    Life cycle functions added

--- SwiftExercises/ViewController.swift
+++ SwiftExercises/ViewController.swift
@@ -19,11 +19,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad function called")
+        
         lblResult.text?.removeAll()
         btnRemove.isHidden=true
         
         lblHeader.text = userNameFromMainScreen
     }
+    
+    //Life cycle functions
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear function called")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear function called")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear function called")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear function called")
+    }
+    
 
    
     @IBAction func btnGetData(_ sender: Any) {
